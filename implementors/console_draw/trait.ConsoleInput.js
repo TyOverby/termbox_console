dc82e8d3a3aed426283fb2485415d27837e0ec6d
Ty Overby <ty@pre-alpha.com>
2014-09-06 16:04:18 -0700

    updated to 1dca188

--- implementors/console_draw/trait.ConsoleInput.js
+++ implementors/console_draw/trait.ConsoleInput.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['console_draw_termbox'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='console_draw/trait.ConsoleInput.html' title='console_draw::ConsoleInput'>ConsoleInput</a> for <a class='struct' href='console_draw_termbox/struct.TermboxConsole.html' title='console_draw_termbox::TermboxConsole'>TermboxConsole</a>",];
+implementors['termbox_console'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='console_draw/trait.ConsoleInput.html' title='console_draw::ConsoleInput'>ConsoleInput</a> for <a class='struct' href='termbox_console/struct.TermboxConsole.html' title='termbox_console::TermboxConsole'>TermboxConsole</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/iter/trait.Iterator.js
+++ implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['console_draw_termbox'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;<a class='type' href='console_draw/type.Update.html' title='console_draw::Update'>Update</a>&gt; for <a class='struct' href='console_draw_termbox/struct.TermboxConsole.html' title='console_draw_termbox::TermboxConsole'>TermboxConsole</a>",];
+implementors['termbox_console'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;<a class='type' href='console_draw/type.Update.html' title='console_draw::Update'>Update</a>&gt; for <a class='struct' href='termbox_console/struct.TermboxConsole.html' title='termbox_console::TermboxConsole'>TermboxConsole</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
